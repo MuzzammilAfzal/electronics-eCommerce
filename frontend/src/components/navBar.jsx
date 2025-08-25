48f6814ea9aa0cf043599c10f6b83473460e23af
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2025-08-25 10:53:58 +0530

    navbar bug

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -5,7 +5,7 @@ function NavBar(){
 
     function SignUpLogin(){
         if(localStorage.getItem("token")){
-          return <button style={{ width: "12%", padding: "10px", background: "blue", color: "white" }} onClick={()=>{
+          return <button style={{ width: "17%", padding: "10px", background: "blue", color: "white" }} onClick={()=>{
           localStorage.removeItem("token")
           location.reload()
         }}>Logout</button>
@@ -17,7 +17,7 @@ function NavBar(){
     return <div>
      <nav style={{display: "flex",justifyContent: "space-evenly",alignItems: "center",flexWrap:"wrap",background: "#333",padding: "10px",color: "white",width:"100vw",}}>
          <h1 >Electronics Store</h1>
-        <div style={{display: "flex",gap: "60px",justifyContent:'center',alignItems:"center"}}>
+        <div style={{display: "flex",gap: "50px",justifyContent:'center',alignItems:"center"}}>
         <Link to="/" style={{color:"white"}}>Home</Link>
         <Link to="/myCart" style={{color:"white"}} >My Cart</Link>
         <Link to="/myOrders" style={{color:"white"}} >My Orders</Link>
