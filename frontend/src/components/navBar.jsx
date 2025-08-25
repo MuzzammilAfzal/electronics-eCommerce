aff5b6139690ff21ad9d623cc20fe6bb81f339cd
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2025-08-25 10:49:39 +0530

    navbar bug

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -17,7 +17,7 @@ function NavBar(){
     return <div>
      <nav style={{display: "flex",justifyContent: "space-evenly",alignItems: "center",flexWrap:"wrap",background: "#333",padding: "10px",color: "white",width:"100vw",}}>
          <h1 >Electronics Store</h1>
-        <div style={{display: "flex",gap: "80px",justifyContent:'center',alignItems:"center"}}>
+        <div style={{display: "flex",gap: "60px",justifyContent:'center',alignItems:"center"}}>
         <Link to="/" style={{color:"white"}}>Home</Link>
         <Link to="/myCart" style={{color:"white"}} >My Cart</Link>
         <Link to="/myOrders" style={{color:"white"}} >My Orders</Link>
