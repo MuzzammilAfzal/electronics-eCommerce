806f5f4e49872319bc8ab4f2e8b396cadf713f44
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2025-08-25 10:31:02 +0530

    bug

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cors=require('cors')
 dotenv.config()
 const app =express()
 app.use(express.json())
-app.use(cors({origin:"http://ec2-51-20-83-24.eu-north-1.compute.amazonaws.com:5000"}))
+app.use(cors({origin:"*"}))
 
 
 app.use('/',userRouter)
@@ -17,6 +17,6 @@ app.use('/admin',adminRouter)
 
 mongoose.connect(process.env.MONGO_URI, {  dbName: "electronicsStore" })
 
-app.listen(4000,()=>{
-    console.log("app running on port 3000")
+app.listen(5000,()=>{
+    console.log("app running on port 5000")
 })
