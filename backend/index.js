3bea113b1e1f69d99a8e38eded737301da2d6e97
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2025-09-30 21:50:44 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,8 @@ const cors=require('cors')
 dotenv.config()
 const app =express()
 app.use(express.json())
-app.use(cors({origin:"*",
-    // methods:["GET","POST","PUT","DELETE"],credentials:true
+app.use(cors({origin:"https://electronics-e-commerce-two.vercel.app/",
+    methods:["GET","POST","PUT","DELETE"],credentials:true
 }))
 
 
