576ee05b190c2cd5823b4497f4aadae950570dcf
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2025-10-06 21:54:35 +0530

    search bar

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -3,12 +3,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./navBar";
 import Footer from "./footer";
+import CategoriesNavBar from "../CategoriesNavBar";
 
 
 function Layout() {
   return (
     <>
       <NavBar/>
+      <CategoriesNavBar/>
       <div className="">
 
         <Outlet /> 
--- frontend/src/components/Layout/navBar.jsx
+++ frontend/src/components/Layout/navBar.jsx
@@ -6,7 +6,7 @@ import Search from "../Search";
 
 function NavBar(){
 
-  const [isMd, setIsMd] = useState(window.innerWidth >= 768);
+  const [isMd, setIsMd] = useState(window.innerWidth >= 768); 
 
     function LoginButton(){
         if(localStorage.getItem("token")){
@@ -16,25 +16,24 @@ function NavBar(){
           location.reload()
         }}>Logout</button>
         }else{
-          return <Link to="/login" className="text-white hover:bg-red-400 rounded-2xl px-2" >Login</Link>
+          return <Link to="/login" className="text-white bg-red-500 hover:bg-red-400 rounded-2xl px-2" >Login</Link>
         }
     }
 
-    return <div className="sticky top-0 z-50  ">
-     <nav className="flex justify-evenly items-center flex-wrap bg-gray-800 md:py-0 text-white w-full h-24  ">
+    return <div className="sticky top-0 z-50">
+     <nav className="flex justify-evenly items-center flex-wrap bg-gray-800 md:py-6 py-2 text-white w-full h-24">
            <h1 className="text-xl font-bold">Electronics Store</h1>
            {isMd && <Search/>}
-     <div className=" flex  gap-10 justify-center items-center">
-         <Link to="/" className="text-white hover:bg-blue-400 rounded-2xl px-2 ">Home</Link>
-         <Link to="/myCart" className="text-white hover:bg-blue-400 rounded-2xl px-2">Cart</Link>
-         <Link to="/myOrders" className="text-white hover:bg-blue-400 rounded-2xl px-2">Orders</Link>
+     <div className="flex py-2 gap-6 justify-center items-center">
+         <Link to="/" className="text-white bg-blue-500 hover:bg-blue-400 rounded-2xl px-2">Home</Link>
+         <Link to="/myCart" className="text-white bg-blue-500 hover:bg-blue-400 rounded-2xl px-2">Cart</Link>
+         <Link to="/myOrders" className="text-white bg-blue-500 hover:bg-blue-400 rounded-2xl px-2">Orders</Link>
       <LoginButton/>
       </div>
      </nav>
      {!isMd && <div className="bg-gray-600 border-white border-2 text-white flex justify-center py-2">
              <Search/>
            </div>}
-    <CategoriesNavBar/>
     </div>
     
 }
