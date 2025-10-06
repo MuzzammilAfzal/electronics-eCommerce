3a792f3e5103f33dd52836b1e39fd336e8c2da20
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2025-10-06 20:48:51 +0530

    search bar

--- frontend/src/components/Layout/navBar.jsx
+++ frontend/src/components/Layout/navBar.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import CategoriesNavBar from "../CategoriesNavBar";
+import Search from "../Search";
 
 
 function NavBar(){
+
+  const [isMd, setIsMd] = useState(window.innerWidth >= 768);
 
     function LoginButton(){
         if(localStorage.getItem("token")){
@@ -16,16 +20,20 @@ function NavBar(){
         }
     }
 
-    return <div className="sticky top-0 z-50">
-     <nav className="flex justify-evenly items-center flex-wrap bg-gray-800 md:py-6 py-2 text-white w-full ">
+    return <div className="sticky top-0 z-50  ">
+     <nav className="flex justify-evenly items-center flex-wrap bg-gray-800 md:py-6 text-white w-full h-24  ">
            <h1 className="text-xl font-bold">Electronics Store</h1>
-     <div className="flex gap-10 justify-center items-center">
-         <Link to="/" className="text-white hover:bg-blue-400 rounded-2xl px-2">Home</Link>
+           {isMd && <Search/>}
+     <div className=" flex  gap-10 justify-center items-center">
+         <Link to="/" className="text-white hover:bg-blue-400 rounded-2xl px-2 ">Home</Link>
          <Link to="/myCart" className="text-white hover:bg-blue-400 rounded-2xl px-2">Cart</Link>
          <Link to="/myOrders" className="text-white hover:bg-blue-400 rounded-2xl px-2">Orders</Link>
       <LoginButton/>
       </div>
      </nav>
+     {!isMd && <div className="bg-gray-600 border-white border-2 text-white flex justify-center py-2">
+             <Search/>
+           </div>}
     <CategoriesNavBar/>
     </div>
     
---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+const url=import.meta.env.VITE_URL
+
+const Search = () => {
+
+    const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const navigate=useNavigate()
+
+  useEffect(() => {
+    if (!query) {
+      setResults([]);
+      return;
+    }
+
+    const delay = setTimeout(() => {
+      fetch(`${url}/search?q=${query}`)
+        .then((res) => res.json())
+        .then((data) => setResults(data))
+        .catch((err) => console.error(err));
+    }, 500);
+
+    return () => clearTimeout(delay);
+  }, [query]);
+  
+    return (
+    <div className=" w-80  " >
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="border p-2 rounded w-full"
+      />
+
+      <ul className="mt-2 border rounded shadow absolute top-22 right-0 left-0 z-50 bg-gray-700">
+        {results.map((item) => (
+             <li key={item._id} className="p-2 hover:bg-gray-500 text-white" onClick={()=>{setQuery("");navigate(`/productPage?${item._id}`) }}>
+            {item.productName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
+export default Search
